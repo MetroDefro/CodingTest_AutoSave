c3e6f51840e63e6c30b0c1572ef046cd87713d37
HEK <65336674+MetroDefro@users.noreply.github.com>
2024-10-31 18:09:55 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1052 ms, Memory: 106188 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+  private static int count = 1;
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    String[] inputs = br.readLine().split(" ");
+    int N = Integer.parseInt(inputs[0]);
+    int M = Integer.parseInt(inputs[1]);
+    int R = Integer.parseInt(inputs[2]);
+
+    List<Integer>[] arr = new ArrayList[N + 1];
+    for(int i = 1; i <= N; i++) {
+      arr[i] = new ArrayList<>();
+    }
+    while (M-- > 0) {
+      inputs = br.readLine().split(" ");
+
+      arr[Integer.parseInt(inputs[0])].add(Integer.parseInt(inputs[1]));
+      arr[Integer.parseInt(inputs[1])].add(Integer.parseInt(inputs[0]));
+    }
+    for(int i = 1; i <= N; i++) {
+      arr[i].sort(Comparator.naturalOrder());
+    }
+
+    int[] order = new int[N + 1];
+    dfs(arr, R, new boolean[N + 1], order);
+    for(int i = 1; i <= N; i++) {
+      bw.write(order[i] + "\n");
+    }
+
+    bw.flush();
+    bw.close();
+
+  }
+
+  private static void dfs(List<Integer>[] arr, int r, boolean[] visited, int[] order) {
+    visited[r] = true;
+    order[r] = count++;
+    for (int k : arr[r]) {
+      if (!visited[k]) {
+        dfs(arr, k, visited, order);
+      }
+    }
+  }
+}
