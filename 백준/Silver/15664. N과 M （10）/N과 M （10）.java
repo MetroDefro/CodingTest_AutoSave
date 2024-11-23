a56190b44bff17b3220b90089eb23fe877a7953b
HEK <65336674+MetroDefro@users.noreply.github.com>
2024-11-23 19:36:06 +0900

    [Silver II] Title: N과 M (10), Time: 120 ms, Memory: 15932 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+  private static int N;
+  private static int M;
+  private static int[] arr;
+  private static int[] temp;
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    arr = new int[N];
+    temp = new int[M];
+    st = new StringTokenizer(br.readLine());
+    for(int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(arr);
+    dfs(0, -1, bw);
+
+    bw.flush();
+    bw.close();
+  }
+
+  private static void dfs(int depth, int prevIndex, BufferedWriter bw) throws IOException {
+    if(depth == M) {
+      for(int i = 0; i < M; i++) {
+        bw.write(temp[i] + " ");
+      }
+      bw.write("\n");
+      return;
+    }
+
+    int prev = 0;
+    for(int i = prevIndex + 1; i < N; i++) {
+      if(arr[i] != prev) {
+        temp[depth] = arr[i];
+        dfs(depth + 1, i, bw);
+      }
+      prev = arr[i];
+    }
+  }
+}
